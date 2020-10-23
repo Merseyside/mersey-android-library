b29287b0e91519bb32516f2a1ee8af1369f27f8a
Иван Саблин <sablin.i@office.lan>
2020-10-23 16:07:17 +0700

    Remove publishing checking

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,13 +3,11 @@ enableFeaturePreview("GRADLE_METADATA")
 val properties = startParameter.projectProperties
 val libraryPublish: Boolean = properties.containsKey("libraryPublish")
 
-if (!libraryPublish) {
-    include(":animators")
-    include(":adapters")
-    include(":utils")
-    include(":app")
-    include(":archy")
-}
+include(":animators")
+include(":adapters")
+include(":utils")
+include(":app")
+include(":archy")
 
 include(":kmp-clean-mvvm-arch")
 include(":kmp-utils")
