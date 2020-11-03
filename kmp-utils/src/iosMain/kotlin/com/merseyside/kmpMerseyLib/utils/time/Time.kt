3afaa0aa4e68a0a9a6c090c88e30ae7e1f49c4ce
Иван Саблин <sablin.i@office.lan>
2020-11-03 12:05:04 +0700

    Add ios native implementation

---
+++ kmp-utils/src/iosMain/kotlin/com/merseyside/kmpMerseyLib/utils/time/Time.kt
@@ -0,0 +1,9 @@
+package com.merseyside.kmpMerseyLib.utils.time
+
+actual fun getCurrentTimeMillis(timeZone: String): Long {
+    TODO("Not yet implemented")
+}
+
+actual fun getFormattedDate(timestamp: Long, pattern: String): String {
+    TODO("Not yet implemented")
+}
---
+++ kmp-utils/src/iosMain/kotlin/com/merseyside/kmpMerseyLib/utils/time/TimeExt.kt
@@ -0,0 +1,5 @@
+package com.merseyside.kmpMerseyLib.utils.time
+
+actual fun String.toTimeUnit(dateFormat: String): TimeUnit {
+    TODO("Not yet implemented")
+}
