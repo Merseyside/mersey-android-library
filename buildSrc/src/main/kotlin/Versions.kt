5b2b50670aa9f0384b30cb8bcb91eae2a9d57e02
Merseyside <ivanklessablin@gmail.com>
2021-09-21 19:14:50 +0700

    Fix android convention plugin

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,8 +4,8 @@ object Application {
     const val groupId = "com.github.Merseyside"
     const val artifactId = "mersey-android-library"
 
-    const val version = "1.4.9"
-    const val versionCode = 149
+    const val version = "1.5.0"
+    const val versionCode = 150
 
     const val compileSdk = 30
     const val targetSdk = 30
