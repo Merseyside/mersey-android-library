17325391afbad767e563e5898fa2c8ed56f5e18d
Ivan Sablin <ivansablin@MacBook-Pro-Ivan.local>
2022-11-30 18:20:25 +0600

    Minor change

--- utils/src/main/java/com/merseyside/utils/binding/ImageBinding.kt
+++ utils/src/main/java/com/merseyside/utils/binding/ImageBinding.kt
@@ -12,6 +12,7 @@ import androidx.databinding.BindingAdapter
 import coil.load
 import coil.request.ImageRequest
 import coil.transform.RoundedCornersTransformation
+import com.merseyside.merseyLib.kotlin.logger.log
 import com.merseyside.merseyLib.kotlin.utils.firstNotNull
 import com.merseyside.merseyLib.kotlin.utils.safeLet
 import com.merseyside.utils.coil.CircleCropStroke
@@ -110,6 +111,7 @@ fun setImageWithCoil(
                 placeholder?.let { placeholder(getValidPlaceholder(it)) }
             }
         } catch (e: NullPointerException) {
+            log("Set placeholder")
             loadPlaceholder(placeholder) { builder() }
         }
     }
